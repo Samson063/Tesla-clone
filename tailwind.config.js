1cc466fb868855e074ec38e8b9affa89de5e2f9c
Samson063 <furthermaths078@gmail.com>
2024-07-17 23:05:25 +0100

    set up tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+
+module.exports = {
+  content: [
+
+    '.Tesla clone/Landing.html',
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        'universal-sans': ['Universal Sans Display', '-apple-system', 'Arial', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+};
